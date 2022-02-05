c8d7370c6727c3cb99b66ac382b802b8dae52f33
sajad sholi <sholi.sajad16@gmail.com>
2022-02-05 19:29:22 +0330

    Set-up-packages

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,22 +6,16 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
+    // Use modules, traits, plugins ...
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // Config the model
+
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +35,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // Filters
+    public function scopeMobile($query, $mobile = null)
+    {
+        if (filled($mobile)) {
+            return $query->whereRaw("LOWER(`mobile`) = ?", [strtolower($mobile)]);
+        }
+        return $query;
+    }
+
+
+    // Relations
+
+
+    // Accessors
+
+
+    // Mutators
+
+
+    // Extra methods
+
 }
